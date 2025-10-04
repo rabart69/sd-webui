20c1c16ffaa6461679a3ffe622c1d263b12f7b1c
rabart69 <rabart@wp.pl>
2025-10-04 20:23:05 +0100

    jajo

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,8 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=   --allow-code --enable-insecure-extension-access ^
+                        --models-dir "d:\_zzz_ai\sd-webui\models"      
+                    
 
 call webui.bat
